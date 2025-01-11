fbbe02db28f86a9675089e87bd5ee1ae5ed73357
justparthi <justparthiban@gmail.com>
2025-01-11 23:26:49 +0530

    Header & Navbar

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favourite food</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod, perspiciatis, repudiandae quasi consectetur aliquam aut reiciendis accusantium fuga voluptatibus est eveniet neque. Obcaecati iste eveniet molestias labore vel omnis nemo.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
