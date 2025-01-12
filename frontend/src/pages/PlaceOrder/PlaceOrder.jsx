ae5bd0f9fb1c9ddc4f95f2214cd2edb1242b08b0
justparthi <justparthiban@gmail.com>
2025-01-12 22:42:54 +0530

    Cart & login page

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -3,8 +3,28 @@ import './PlaceOrder.css'
 
 const PlaceOrder = () => {
   return (
-    <div>
-      
+    <div className='place-order'>
+      <div className="place-order-left">
+      <p className="title">Delivery Information</p>
+      <div className="multi-fields">
+        <input type="text" placeholder='First Name'/>
+        <input type="text" placeholder='Last Name' />
+      </div>
+      <input type="text" placeholder='email address' />
+      <input type="text" placeholder='street' />
+      <div className="multi-fields">
+        <input type="text" placeholder='city'/>
+        <input type="text" placeholder='state'/>
+      </div>
+      <div className="multi-fields">
+        <input type="text" placeholder='zipcode'/>
+        <input type="text" placeholder='country' />
+      </div>
+      <input type="text" placeholder='phone'/>
+      </div>
+      <div className="place-order-right">
+
+      </div>
     </div>
   )
 }
