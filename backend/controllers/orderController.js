6bc7d9e1667e3712e2733084f1bc7535384d0de3
justparthi <justparthiban@gmail.com>
2025-01-14 16:05:53 +0530

    Orders Features

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -76,8 +76,20 @@ const verifyOrder = async (req,res) => {
         console.log(error);
         res.json({success:false,message:"Error"})
         
+    }
+
+
+}
+
+const userOrders = async (req,res) => {
+    try {
+        const orders = await orderModel.find({userId:req.body.userId})
+        res.json({success:true,data:orders})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
         
     }
 }
 
-export {placeOrder, verifyOrder}
+export {placeOrder, verifyOrder, userOrders}
